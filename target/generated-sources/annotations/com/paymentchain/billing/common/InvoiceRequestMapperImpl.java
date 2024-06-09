1ef55096069b2505fcfae23a817e1f57f1e70425
mauromamani <mauromamani.b@gmail.com>
2024-06-09 14:28:21 -0300

    123

--- target/generated-sources/annotations/com/paymentchain/billing/common/InvoiceRequestMapperImpl.java
+++ target/generated-sources/annotations/com/paymentchain/billing/common/InvoiceRequestMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-06-09T14:19:43-0300",
+    date = "2024-06-09T14:25:35-0300",
     comments = "version: 1.3.1.Final, compiler: Eclipse JDT (IDE) 3.38.0.v20240524-2033, environment: Java 17.0.11 (Eclipse Adoptium)"
 )
 @Component
